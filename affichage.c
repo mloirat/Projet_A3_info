1f06bd6551095d7bfeb8ba512f25e220de7c57c6
mloirat <marie.loirat@hotmail.fr>
2020-04-06 18:06:56 +0200

    fonction affichage

--- affichage.c
+++ affichage.c
@@ -1,7 +1,21 @@
 #include "affichage.h"
-
+#include <stdio.h>
+#include <unistd.h>
 void affichage(oxy myOxy){
+    if( access( ".verrouData", F_OK ) != -1 ){ // Fichier verrou existe
+        printf("Attention l'application java lit le Data.txt");//on supprime le fichier verrou pour que l'application java puisse lire le fichier Data.txt
 
-		
+    }else{// Fichier verrou n'existe pas
+        FILE* verrou = fopen(".verrouData", "r");;//ce fichier va permettre de bloquer la lecture de l'application java sur le fichier Data.txt
+        //"r" = ouverture en lecture seule
+
+        FILE* fichier_data = fopen("Data.txt", "w+"); //le fichier data.txt où l'on va écrire les nouvelles valeurs de SpO2 et BMP
+        //"w+"= ouverture en écriture et lecture, en effaçant au préalable les anciennes valeurs
+        if (fichier_data != NULL){  //on peut lire le fichier Data.txt
+            fprintf(fichier_data, "%i\n%i", myOxy.spo2,myOxy.pouls); //on écrit les nouvelles valeurs de Sp02 et BMP dans le fichier Data.txt
+            fclose(fichier_data); //on referme le fichier data.txt
+        }
+        remove(".verrouData");
+    }
 }
 
--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
+#include "affichage.h"
+#include "define.h"
 int main() {
     printf("Hello, World!\n");
+    oxy myOxygene ;
+    myOxygene.spo2=91;
+    myOxygene.pouls=130;
+    affichage(myOxygene);
     return 0;
 }
